3640b8c9587f2a454fcacdc77d361a936c94aa47
GonzaloSrZ <gon.srz@gmail.com>
2022-11-14 21:27:14 -0300

    requirecontext

--- resources/views/evaluacion.blade.php
+++ resources/views/evaluacion.blade.php
@@ -9,12 +9,24 @@
 <div class="container mx-auto flex justify-center">
     -
 </div>
+
+ {{-- {{importAll(require.context('../public/', true));}} --}}
 <div style="width: 100%;">
     <div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;">
-        <iframe title="Taller 2" frameborder="0" width="1200px" height="675px"
+        <iframe title="Taller 2" id="frame1" frameborder="0" width="1200px" height="675px"
             style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
-            src="/evaluacion/index.htm" type="text/html" allowscriptaccess="always"
+            src="" type="text/html" allowscriptaccess="always"
             allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
     </div>
 </div>
+
+<script>
+    const req=require.context('./public/evaluacion', useSubdirectories = true)
+
+    const index= req('./index.htm')
+    
+    var iframe = document.getElementById("frame1");
+
+    iframe.setAttribute("src", index);
+</script>
 @stop

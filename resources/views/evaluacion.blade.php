145c724e605608922a42161b114e8bb466c0c507
GonzaloSrZ <gon.srz@gmail.com>
2022-11-16 11:42:48 -0300

    asset

--- resources/views/evaluacion.blade.php
+++ resources/views/evaluacion.blade.php
@@ -13,7 +13,7 @@
     {{-- {{importAll(require.context('../public/', true));}} --}}
     <div style="width: 100%;">
         <div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;">
-            <iframe title="Taller 2" id="frame1" frameborder="0" width="1200px" height="675px"
+            <iframe title="Taller 2" id="frame1" frameborder="0" width="1200px" height="675px" src="{{asset('/evaluacion/index.htm')}}"
                 style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" type="text/html"
                 allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
         </div>
@@ -26,12 +26,13 @@
 
             var ruta = "../evaluacion/index.htm";
 
+
             var iframe = document.getElementById("frame1");
 
             iframe.setAttribute("src", ruta);
 
         }
-        prueba();
+        //prueba();
 
     </script>
 

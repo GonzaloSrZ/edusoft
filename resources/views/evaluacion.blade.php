8395901a96d2e5c5996569ef4b92790e0db44045
GonzaloSrZ <gon.srz@gmail.com>
2022-11-16 11:25:15 -0300

    public

--- resources/views/evaluacion.blade.php
+++ resources/views/evaluacion.blade.php
@@ -2,31 +2,37 @@
 
 @section('content')
 
-<h2 style="color:green">
-     
-</h2>
+    <h2 style="color:green">
 
-<div class="container mx-auto flex justify-center">
-    -
-</div>
+    </h2>
 
- {{-- {{importAll(require.context('../public/', true));}} --}}
-<div style="width: 100%;">
-    <div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;">
-        <iframe title="Taller 2" id="frame1" frameborder="0" width="1200px" height="675px"
-            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
-            src="" type="text/html" allowscriptaccess="always"
-            allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
+    <div class="container mx-auto flex justify-center">
+        -
     </div>
-</div>
 
-<script>
-    const req=require.context('./public/evaluacion', useSubdirectories = true)
+    {{-- {{importAll(require.context('../public/', true));}} --}}
+    <div style="width: 100%;">
+        <div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;">
+            <iframe title="Taller 2" id="frame1" frameborder="0" width="1200px" height="675px"
+                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" type="text/html"
+                allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
+        </div>
+    </div>
 
-    const index= req('./index.htm')
-    
-    var iframe = document.getElementById("frame1");
+    <script>
+        // importAll(require.context('/evaluacion', false));
 
-    iframe.setAttribute("src", index);
-</script>
+        function prueba() {
+
+            var ruta = "../evaluacion/index.htm";
+
+            var iframe = document.getElementById("frame1");
+
+            iframe.setAttribute("src", ruta);
+
+        }
+        prueba();
+
+    </script>
+
 @stop

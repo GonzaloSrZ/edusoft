e75b5fa2faf27d613d30f7faed0c483a5269bc89
GonzaloSrZ <gon.srz@gmail.com>
2022-11-24 21:56:55 -0300

    agr ejerc

--- app/Http/Livewire/Vectores.php
+++ app/Http/Livewire/Vectores.php
@@ -12,18 +12,38 @@ class Vectores extends Component
 
     public $b=true;
 
+    public $opc=1;
+
+    public $msj="";
+
 
     public function render()
     {
         if ($this->b){
-            $this->genVector();
+            $this->opGen();
+            $this->b=false;
         }
         return view('livewire.vectores');
     }
 
+    public function opGen(){
+        $this->opc = mt_rand(1, 2);
+        switch ($this->opc) {
+            case 1:
+                $this->insertar();
+                break;
+            case 2:
+                $this->añadir();
+                break;
+            case 2:
+                echo "i es igual a 2";
+                break;
+        }
+
+    }
+
     public function genVector()
     {
-        $this->b=false;
         $this->arreglo = [];
         $this->arreglo = collect($this->arreglo);
 
@@ -38,9 +58,71 @@ class Vectores extends Component
         $this->reset('n1','n2','n3','n4','n5',);
     }
 
-    public function verificar()
-    {
+    public function insertar(){
+        $this->genVector();
+        $this->arreglo = collect($this->arreglo)->toArray();
+        
+        
+        sort($this->arreglo);
+
+        $this->arreglo = collect($this->arreglo);
+        //$array = collect($array);
+        
+        //$this->arreglo=collect($array);
+        //$this->arreglo = collect($this->arreglo);
+
+        $this->msj="El vector generado se encuentra ordenado y vamos a INSERTAR el siguiente valor: ";
+
+    }
+
+    public function verifInsertar(){
         $arregloAux = collect($this->arreglo);
+        $arregloAux->push($this->valor);
+        
+        
+        
+        $arregloAux = collect($arregloAux)->toArray();
+        sort($arregloAux);
+        $arregloAux = collect($arregloAux);
+
+
+        $this->verificar($arregloAux);
+    }
+
+
+    public function añadir(){
+        $this->genVector();
+        $this->arreglo = collect($this->arreglo);
+
+        $this->msj="En el vector que se ha generado vamos a AÑADIR el siguiente valor: ";
+
+    }
+    
+    public function verifAñadir(){
+        $arregloAux = collect($this->arreglo);
+        $arregloAux->push($this->valor);
+        $this->verificar($arregloAux);
+
+    }
+
+    public function opVerif(){
+        switch ($this->opc) {
+            case 1:
+                $this->verifInsertar();
+                break;
+            case 2:
+                $this->verifAñadir();
+                break;
+            case 2:
+                echo "i es igual a 2";
+                break;
+        }
+
+    }
+
+    public function verificar($arregloAux)
+    {
+        
         $control = [];
         $control = collect($control);
 
@@ -50,7 +132,7 @@ class Vectores extends Component
         $control->push($this->n4);
         $control->push($this->n5);
 
-        $arregloAux->push($this->valor);
+        
         if ($control==$arregloAux) {
             $this->emit('alert', 'El vector está creado correctamente');
         } else {
--- resources/views/livewire/vectores.blade.php
+++ resources/views/livewire/vectores.blade.php
@@ -1,29 +1,29 @@
 <div>
-    <p>Según el siguiente vector: [{{ implode(',', collect($arreglo)->toArray()) }}]</p>
-    <p>Ingrese como quedaría el nuevo vector añadiendo el valor: {{ $valor }}</p>
+    <p>Según el siguiente vector: <b>[{{ implode(',', collect($arreglo)->toArray()) }}]</b></p>
+    <p>{{ $msj }}<b>{{$valor}}</b></p>
     <div class="row g-5">
-            <div class="col-sm-1">
+            <div class="" style="width: 110px">
                 <input type="number" wire:model.defer="n1" class="form-control">
             </div>
-            <div class="col-sm-1">
+            <div class="" style="width: 110px">
                 <input type="number" wire:model.defer="n2" class="form-control">
             </div>
-            <div class="col-sm-1">
+            <div class="" style="width: 110px">
                 <input type="number" wire:model.defer="n3" class="form-control">
             </div>
-            <div class="col-sm-1">
+            <div class="" style="width: 110px">
                 <input type="number" wire:model.defer="n4" class="form-control">
             </div>
-            <div class="col-sm-1">
+            <div class="" style="width: 110px">
                 <input type="number" wire:model.defer="n5" class="form-control">
             </div>
     </div>
 
     <br>
 
-    <button class="btn btn-secondary text-light" wire:click="genVector">Generar</button>
+    <button class="btn btn-secondary text-light" wire:click="opGen">Generar otro</button>
 
-    <button class="btn btn-dark text-light" wire:click="verificar">Verificar</button>
+    <button class="btn btn-dark text-light" wire:click="opVerif">Verificar</button>
 
     @section('js')
         <script>

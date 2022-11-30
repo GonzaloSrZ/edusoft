dd7f7ae6d4cd9197e93e74de51b7f7a454b1603c
GonzaloSrZ <gon.srz@gmail.com>
2022-11-30 12:20:10 -0300

    dandole forma index y aprender

--- resources/views/aprender.blade.php
+++ resources/views/aprender.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h2 class="text-secondary">
+    <h2 class="text-primary text-center">
         Clase sobre arreglos unidimensionales y bidimensionales
     </h2>
 
@@ -11,12 +11,29 @@
         recursos proporcionados a continuación
     </p>
 
-    <div style="width: 100%;">
-        <div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;">
-            <iframe title="Taller 2" frameborder="0" width="1200px" height="675px"
-                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
-                src="https://view.genial.ly/63515fe21bf18000180e2df2" type="text/html" allowscriptaccess="always"
-                allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
+    <div class="card w-100">
+        <div class="card-body">
+            <h3 class="card-title text-secondary text-center">
+                Vectores
+            </h3>
+            <div style="width: 100%;">
+                <div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;">
+                    <iframe title="Taller 2" frameborder="0" width="1200px" height="675px"
+                        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
+                        src="https://view.genial.ly/63515fe21bf18000180e2df2" type="text/html" allowscriptaccess="always"
+                        allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<br>
+    <div class="card w-100">
+        <div class="card-body">
+            <h3 class="card-title text-secondary text-center">
+                Matrices
+            </h3>
+
         </div>
     </div>
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,41 @@
 
 @section('content')
 
-<h2 class="text-secondary">
-    Bienvenidos al "Software Educativo para la Enseñanza de Arreglos"
-</h2>
+    <h2 class="text-primary text-center">
+        Bienvenidos al "Software Educativo para la Enseñanza de Arreglos"
+    </h2>
+    <br>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title text-secondary">Introducción</h5>
+                    <p class="card-text">Está aplicación está elaborada con el fin de poder obtener los conocimientos necesarios
+                        sobre los arreglos. A continuación podrá ver las secciones que se incluyen.
+                    </p>
+                </div>
+            </div>
+            <br>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title text-secondary">Contenido</h5>
+                    <p class="card-text"> <a href="{{ route('aprender') }}" class="link-secondary">Aprendé:</a>
+                         Teoría basica sobre Vectores y Matrices.
+                    </p>
+                    <p class="card-text"> <a href="{{ route('practicar') }}" class="link-secondary">Practicá:</a>
+                         Ejercicios que se autogeneran para aplicar los conocimientos obtenidos.
+                    </p>
+                    <p class="card-text"> <a href="{{ route('evaluar') }}" class="link-secondary">Evaluate:</a>
+                         Juegos autoevaluativos para calificar nuestros nuevos conocimientos sobre los temas.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <iframe src="https://giphy.com/embed/MDxPrqREJbYFX5Cvav" width="480" height="480" frameBorder="0"
+                class="giphy-embed" allowFullScreen></iframe>
+        </div>
+    </div>
 
 @stop
